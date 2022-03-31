177263406c1f00f3f033b08cd0bf03fd1e73284d
KirillKalutsky <kefirchik.2000@mail.ru>
2022-03-31 16:40:08 +0500

    реализация параллельного клиента

--- ClusterClient/Clients/ParallelClusterClient.cs
+++ ClusterClient/Clients/ParallelClusterClient.cs
@@ -1,22 +1,49 @@
 ﻿using System;
+using System.Formats.Asn1;
+using System.Linq;
 using System.Threading.Tasks;
 using ClusterClient.Clients;
 using log4net;
 
-namespace ClusterTests;
-
-public class ParallelClusterClient : ClusterClientBase
+namespace ClusterTests
 {
-	public ParallelClusterClient(string[] replicaAddresses)
-		: base(replicaAddresses)
-	{
-		throw new NotImplementedException();
-	}
 
-	public override Task<string> ProcessRequestAsync(string query, TimeSpan timeout)
-	{
-		throw new NotImplementedException();
-	}
+    public class ParallelClusterClient : ClusterClientBase
+    {
+        public ParallelClusterClient(string[] replicaAddresses)
+            : base(replicaAddresses)
+        {
+            
+        }
 
-	protected override ILog Log => LogManager.GetLogger(typeof(ParallelClusterClient));
+        public override async Task<string> ProcessRequestAsync(string query, TimeSpan timeout)
+        {
+            var requests = ReplicaAddresses
+                .Select
+                    (
+                        async add =>
+                        {
+                            var task = ProcessRequestAsync(CreateRequest($"{add}?query={query}"));
+                            await Task.WhenAny(task, Task.Delay(timeout));
+                            if (!task.IsCompleted)
+                                throw new TimeoutException();
+                            return task;
+                        }
+                    ).ToList();
+
+            while (requests.Any())
+            {
+                var t = await Task.WhenAny(requests);
+                requests.Remove(t);
+                var r = await t;
+                if(r.Status==TaskStatus.Faulted)
+                    continue;
+                return await r;
+            }
+
+            throw new TimeoutException();
+        }
+
+        protected override ILog Log => LogManager.GetLogger(typeof(ParallelClusterClient));
+    }
 }
