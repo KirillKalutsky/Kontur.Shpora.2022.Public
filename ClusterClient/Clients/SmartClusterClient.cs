87690d9c9f38a7fa50239083fd6c5adbd93125fc
KirillKalutsky <kefirchik.2000@mail.ru>
2022-03-31 16:41:58 +0500

    реализация smartClient

--- ClusterClient/Clients/SmartClusterClient.cs
+++ ClusterClient/Clients/SmartClusterClient.cs
@@ -1,22 +1,76 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using System.Threading.Tasks;
 using ClusterClient.Clients;
 using log4net;
 
-namespace ClusterTests;
-
-public class SmartClusterClient : ClusterClientBase
+namespace ClusterTests
 {
-	public SmartClusterClient(string[] replicaAddresses)
-		: base(replicaAddresses)
-	{
-		throw new NotImplementedException();
-	}
 
-	public override Task<string> ProcessRequestAsync(string query, TimeSpan timeout)
-	{
-		throw new NotImplementedException();
-	}
+    public class SmartClusterClient : ClusterClientBase
+    {
+        private readonly Dictionary<string, List<long>> previous = new ();
 
-	protected override ILog Log => LogManager.GetLogger(typeof(SmartClusterClient));
+        public SmartClusterClient(string[] replicaAddresses)
+            : base(replicaAddresses)
+        {
+            
+        }
+
+        public override async Task<string> ProcessRequestAsync(string query, TimeSpan timeout)
+        {
+            List<Task<Task<string>>> requests = new();
+
+            var dt = timeout / ReplicaAddresses.Length;
+
+            IEnumerable<string> addresses;
+            lock (previous)
+                addresses = previous.OrderBy(x => x.Value.Sum() / x.Value.Count)
+                    .Select(x => x.Key)
+                    .Concat(ReplicaAddresses.Where(x => !previous.ContainsKey(x)));
+
+            foreach (var add in addresses)
+            {
+                var webRequest = CreateRequest(add + "?query=" + query);
+
+                var timer = new Stopwatch();
+                timer.Start();
+                var resultTask = ProcessRequestAsync(webRequest).ContinueWith(req =>
+                {
+                    timer.Stop();
+                    lock (previous)
+                    {
+                        if (previous.ContainsKey(add))
+                            previous[add].Add(timer.ElapsedMilliseconds);
+                        else
+                            previous[add] = new List<long> { timer.ElapsedMilliseconds };
+                    }
+                    return req;
+                });
+
+                requests.Add(resultTask);
+
+                var fastRequest = Task.WhenAny(requests);
+                await Task.WhenAny(fastRequest, Task.Delay(dt));
+
+                if (fastRequest.IsCompleted)
+                {
+                    var task = await fastRequest;
+                    requests.Remove(task);
+                    if (task.Status != TaskStatus.Faulted)
+                    {
+                        var result = await task;
+                        if (result.Status != TaskStatus.Faulted)
+                            return await result;
+                    }
+                }
+            }
+            throw new TimeoutException();
+        }
+
+        protected override ILog Log => LogManager.GetLogger(typeof(SmartClusterClient));
+    }
 }
