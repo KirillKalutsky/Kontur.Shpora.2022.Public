38b5bac69bcfa5e5193ff50d3275ce10c5e5220a
KirillKalutsky <kefirchik.2000@mail.ru>
2022-04-01 13:10:13 +0500

    исправил ошибки, убрал вложенные таски

--- ClusterClient/Clients/SmartClusterClient.cs
+++ ClusterClient/Clients/SmartClusterClient.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -12,63 +11,106 @@ namespace ClusterTests
 
     public class SmartClusterClient : ClusterClientBase
     {
-        private readonly Dictionary<string, List<long>> previous = new ();
+        private const int historyLength = 100;
+        private readonly Dictionary<string, Queue<long>> previousAttempts;
+        private readonly Dictionary<Task, Tuple<long,string>> startLog = new();
 
         public SmartClusterClient(string[] replicaAddresses)
             : base(replicaAddresses)
         {
-            
+            previousAttempts = new(replicaAddresses.Length);
         }
 
         public override async Task<string> ProcessRequestAsync(string query, TimeSpan timeout)
         {
-            List<Task<Task<string>>> requests = new();
+            var exceptions = new List<Exception>();
+            List<Task<string>> requests = new();
+            var replicaCount = ReplicaAddresses.Length;
+            var timeoutTimer = new Stopwatch();
+            var addresses = GetOptimalReplicaOrder();
+            
+            timeoutTimer.Start();
 
-            var dt = timeout / ReplicaAddresses.Length;
-
-            IEnumerable<string> addresses;
-            lock (previous)
-                addresses = previous.OrderBy(x => x.Value.Sum() / x.Value.Count)
-                    .Select(x => x.Key)
-                    .Concat(ReplicaAddresses.Where(x => !previous.ContainsKey(x)));
-
-            foreach (var add in addresses)
+            foreach (var address in addresses)
             {
-                var webRequest = CreateRequest(add + "?query=" + query);
+                var timeLimit = (timeout - timeoutTimer.Elapsed) / replicaCount;
+                if(timeLimit.TotalMilliseconds == 0)
+                    break;
+                replicaCount--;  
 
-                var timer = new Stopwatch();
-                timer.Start();
-                var resultTask = ProcessRequestAsync(webRequest).ContinueWith(req =>
+                var webRequest = CreateRequest(address + "?query=" + query);
+                var request = ProcessRequestAsync(webRequest);
+
+                lock (startLog)
+                    startLog[request] = Tuple.Create(timeoutTimer.ElapsedMilliseconds, address);
+                requests.Add(request);
+
+                var border = Task.Delay(timeLimit);
+                await Task.WhenAny(new [] {border}.Concat(requests));
+
+                if (border.IsCompleted)
+                    continue;
+
+                var fastRequest = await Task.WhenAny(requests);
+                requests.Remove(fastRequest);
+
+                WriteTime(fastRequest, timeoutTimer.ElapsedMilliseconds);
+
+                if (fastRequest.IsCompletedSuccessfully)
+                    return await fastRequest;
+
+                exceptions.Add(fastRequest.Exception);
+            }
+
+            if (exceptions.Any())
+            {
+                exceptions.Add(new TimeoutException());
+                throw new AggregateException(exceptions);
+            }
+
+            throw new TimeoutException();
+        }
+
+
+        private void WriteTime(Task fastRequest, long currentTime)
+        {
+            Tuple<long, string> taskValue;
+            lock (startLog)
+                taskValue = startLog[fastRequest];
+
+            var address = taskValue.Item2;
+            var workTime = currentTime - taskValue.Item1;
+
+            lock (previousAttempts)
+            {
+                if (previousAttempts.ContainsKey(address))
                 {
-                    timer.Stop();
-                    lock (previous)
+                    if (previousAttempts.Count < historyLength)
+                        previousAttempts[address].Enqueue(workTime);
+                    else
                     {
-                        if (previous.ContainsKey(add))
-                            previous[add].Add(timer.ElapsedMilliseconds);
-                        else
-                            previous[add] = new List<long> { timer.ElapsedMilliseconds };
-                    }
-                    return req;
-                });
-
-                requests.Add(resultTask);
-
-                var fastRequest = Task.WhenAny(requests);
-                await Task.WhenAny(fastRequest, Task.Delay(dt));
-
-                if (fastRequest.IsCompleted)
-                {
-                    var task = await fastRequest;
-                    requests.Remove(task);
-                    if (task.Status != TaskStatus.Faulted)
-                    {
-                        var result = await task;
-                        if (result.Status != TaskStatus.Faulted)
-                            return await result;
+                        previousAttempts[address].Dequeue();
+                        previousAttempts[address].Enqueue(workTime);
                     }
                 }
+                else
+                {
+                    var newHistory = new Queue<long>(historyLength);
+                    newHistory.Enqueue(workTime);
+                    previousAttempts[address] = newHistory;
+                }
             }
-            throw new TimeoutException();
+        }
+
+        private IEnumerable<string> GetOptimalReplicaOrder()
+        {
+            lock (previousAttempts)
+            {
+                return previousAttempts
+                    .OrderBy(pair => pair.Value.Sum() / pair.Value.Count)
+                    .Select(pair => pair.Key)
+                    .Concat(ReplicaAddresses.Where(addr => !previousAttempts.ContainsKey(addr)));
+            }
         }
 
         protected override ILog Log => LogManager.GetLogger(typeof(SmartClusterClient));
