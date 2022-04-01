cffb1bc1b80977aabec1ec66034206de24ee3a7b
KirillKalutsky <kefirchik.2000@mail.ru>
2022-04-01 13:08:23 +0500

    исправил сравнение состояний таски, и сохранил ошибки

--- ClusterClient/Clients/ParallelClusterClient.cs
+++ ClusterClient/Clients/ParallelClusterClient.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Formats.Asn1;
 using System.Linq;
 using System.Threading.Tasks;
@@ -18,27 +20,37 @@ namespace ClusterTests
 
         public override async Task<string> ProcessRequestAsync(string query, TimeSpan timeout)
         {
+            var timer = new Stopwatch();
+            var exceptions = new List<Exception>();
+            timer.Start();
             var requests = ReplicaAddresses
-                .Select
-                    (
-                        async add =>
-                        {
-                            var task = ProcessRequestAsync(CreateRequest($"{add}?query={query}"));
-                            await Task.WhenAny(task, Task.Delay(timeout));
-                            if (!task.IsCompleted)
-                                throw new TimeoutException();
-                            return task;
-                        }
-                    ).ToList();
+                .Select(address => ProcessRequestAsync(CreateRequest($"{address}?query={query}")))
+                .ToList();
 
             while (requests.Any())
             {
-                var t = await Task.WhenAny(requests);
-                requests.Remove(t);
-                var r = await t;
-                if(r.Status==TaskStatus.Faulted)
-                    continue;
-                return await r;
+                var timeLimit = timeout - timer.Elapsed;
+                if(timeLimit.TotalMilliseconds == 0)
+                    break;
+                var border = Task.Delay(timeLimit);
+                await Task.WhenAny(new []{border}.Concat(requests));
+
+                if(border.IsCompleted)
+                    break;
+
+                var fastRequest = await Task.WhenAny(requests);
+                requests.Remove(fastRequest);
+                
+                if (fastRequest.IsCompletedSuccessfully)
+                    return await fastRequest;
+
+                exceptions.Add(fastRequest.Exception);
+            }
+
+            if (exceptions.Any())
+            {
+                exceptions.Add(new TimeoutException());
+                throw new AggregateException(exceptions);
             }
 
             throw new TimeoutException();
